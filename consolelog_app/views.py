9bcf7811be6e729654fbc445af83a2dc4f10b953
Kbeach321 <iamkevinbeach@gmail.com>
2018-07-18 12:06:39 -0400

    cleanup

--- consolelog_app/views.py
+++ consolelog_app/views.py
@@ -5,8 +5,6 @@ from rest_framework import generics
 from consolelog_app.models import Timestamp
 from consolelog_app.serializers import TimestampSerializer
 
-# Create your views here.
-
 
 class IndexView(TemplateView):
     template_name = "index.html"
