afa9b5a05d459624df6997b3e7aae55b37fce20a
Kbeach321 <iamkevinbeach@gmail.com>
2018-07-18 10:20:39 -0400

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn consolelog.wsgi
--- consolelog/settings.py
+++ consolelog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '44=2m(mv+m$gk8j0m3pw$85&a8u0si9^-89936pc93!0xq73ju'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
