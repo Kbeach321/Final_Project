7834a5de7c644d2036ac2745a2e63e99039b7672
Kbeach321 <iamkevinbeach@gmail.com>
2018-08-16 19:03:42 -0400

    fixed Title

--- static/src/index.js
+++ static/src/index.js
@@ -19,11 +19,7 @@ import Login from './components/Log-in/Log-in.js'
 
 const history = createBrowserHistory()
 
-<head>
-  <title>
-      Console.Log
-  </title>
-</head>
+
 
 ReactDOM.render(
   <Router history={history}>
